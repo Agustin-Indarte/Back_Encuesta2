374c7ab4920fe3bab0e907a704f815af131744bd
martinsosa5 <martinsoosaa31@gmail.com>
2025-08-14 20:37:58 -0300

    agregue multer para el procesamiento de imagenes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,32 +1,215 @@
-export const home = (req,res) => {
-    res.send("Esta es la ruta home para los usuarios") // ruta principal del home
-}
+import User from '../models/user.model.js';
+import bcrypt from "bcryptjs";
+import crypto from 'crypto';
+import { createAccessToken } from '../helpers/jwt.js';
+import transport from '../helpers/mailer.js';
+import jwt from 'jsonwebtoken';
 
-export const about = (req, res) => {
-    res.send("Esta es la ruta de about")
-}
 
-export const getUser = (req,res) => {
+export const register = async(req, res) => {
+
     try {
-      const {username, email, fecha_nac, password} = req.body; //obtengo el usuario del body de la peticion
 
-      if(!username || !email || !fecha_nac || !password){
-        return res.status(400).json({message:"Todos los datos del usuario son obligatorios"}) //si no se envia el usuario, devuelvo un error 400
-      }     
+       const {username, email, password} = req.body; //Preparo la información que envía el cliente
 
-      console.log(req.body);
-      
-      return res.status(200).json({
-        user: {
-            username,
-            email,
-            fecha_nac,
-            password
-        }, //devuelvo el usuario
-        message: "Usuario obtenido correctamente" //mensaje de exito 
-      })         
+       const userFound = await User.findOne({email}); // pregunto a mongo si existe un usuario con el email que envia el cliente
+
+       if(userFound) return res.status(400).json({message: "User already Exist"}); // si existe retornamos mensaje de error
+       
+       const passwordHash = await bcrypt.hash(password, 12); // hasheo la pass con un salt de 12
+
+       //Generar token de verificación de correo
+       const verificationToken = crypto.randomBytes(20).toString('hex');
+        
+       const newUser = new User({
+        username, 
+        email, 
+        password:passwordHash,
+        verificationToken,
+       })
+
+       const savedUser = await newUser.save(); //gurado el nuevo usuario en la base de datos
+
+       // 1 - Enviar el email para la verificación
+       const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${verificationToken}`
+
+       await transport.sendMail({
+        from: process.env.MAIL_FROM,
+        to: savedUser.email,
+        subjet: 'Verifica tu email - ROLLING TODO_APP',
+        template: 'verifyEmail',
+        context:{
+            username: savedUser.username,
+            verificationLink //aca le paso las variables para que las use en la plantilla despues
+        }
+       });
+
+       // 2 - crear el token
+       const token = await createAccessToken({
+            id: savedUser._id,
+            username: savedUser.username,
+            email: savedUser.email
+       });
+
+       // 3 - Responder al cliente con la cookie(es el dato que necesita el usuario para persistir la sesion)
+       res.cookie("token", token, {
+            httpOnly: process.env.NODE_ENV !== "development",
+            secure: true,
+            sameSite: "none",
+       })
+
+       // 4 -  enviar la respuesta al cliente
+       res.status(201).json({
+        id: savedUser._id,
+        username: savedUser.username,
+        email: savedUser.email,
+        isVerified: savedUser.isVerified,
+        token,
+       })
+
+        
     } catch (error) {
         console.log(error);
-        res.status(500).json({message:"Error obteniendo el usuario"})        
+        return res.status(500).json({message: error.message})        
+    }
+  
+
+}
+
+export const login = async(req,res) => {
+    try {
+
+        const {email, password} = req.body; // destructuro el request body
+
+        const userFound = await User.findOne({email}); // busco si el usuario existe en mongo
+
+        if(!userFound) return res.status(400).json({message: "user not found"}) // si el usuario no existe devuelvo error
+
+        //comparo la password enviada contra la guardada en mongo
+        const isMatch = await bcrypt.compare(password, userFound.password) // comparo la constraseña enviada contra la del usuario encontrado en mongo
+
+        if(!isMatch) return res.status(400).json({message: "Invalid Credentials"}) // si las contraseñas no son iguales doy mensaje de error
+
+        //crear un token para el usuario
+        const token = await createAccessToken({
+            id:userFound._id,
+            username: userFound.username,
+            email: userFound.email
+        })
+
+        //setear la cookie
+        res.cookie("token", token, {
+            httpOnly:true,
+            secure:true,
+            sameSite:"none",
+        })
+
+        //enviar la respuesta al cliente
+        res.status(200).json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            isVerified: userFound.isVerified,
+            token
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: error.message})    
     }
 }
+
+export const logout = async (req,res) => {
+    try {
+      //limpiar la cookie para matar la sesión 
+       res.cookie("token", "", {expires: new Date(0)}) // limpiar seteandola con un string vacio y también con una fecha de expiración que está en el pasado
+       res.status(200).json({message: "Logout Success!"})
+    } catch (error) {
+         console.log(error);
+        return res.status(500).json({message: error.message})  
+    }
+}
+
+export const profile = async (req,res) => {
+    try {
+        const userFound = await User.findById(req.user.id);
+      
+        if(!userFound) return res.status(404).json({message: "user not found"})
+
+        return res.status(200).json({
+            id: userFound.id,
+            username: userFound.username,
+            email: userFound.email,
+            profileImage: userFound.profileImage,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt
+        })    
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: error.message})  
+    }
+}
+//para preguntar si el token sigue vivo
+export const verifyToken = async(req,res) => {
+    try {      
+        let token;
+
+         if(!token && req.cookies?.token){ //Valida si NO hay token en memoria pero sí hay un token en las cookies
+            token = req.cookies.token
+        } else{
+            return res.status(401).json({meesage: "No token provided"})
+        }
+
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const userFound = await User.findById(decoded.id);
+
+        if(!userFound) return res.status(401);
+
+        return res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            isVerified: userFound.isVerified
+        })        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: error.message})  
+    }
+}
+
+export const verifyEmail = async (req,res) => {
+    try {
+
+     const { token } = req.query; 
+
+     const user = await User.findOne({verificationToken:token}); //compruebo de que exista el mismo token
+
+     if(!user) {
+        return res.status(400).json({message: "Invalid or Expired Token"})
+     }
+
+     user.isVerified = true;
+     user.verificationToken= undefined;
+     await user.save(); //actualizo la base de datos
+
+     return res.status(200).json({
+        success: true,
+        message: "Verify email success!",
+        user:{
+            id:user._id,
+            username: user.username,
+            email: user.email,
+            isVerified: user.isVerified
+        }
+     })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: error.message})  
+    }
+}
+
+
+
+
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,29 @@
 import express from 'express'; //importo express
-import { about, getUser, home } from '../controllers/auth.controller.js';
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { loginSchema, registerSchema } from '../validators/auth.validator.js';
+import {login, logout, profile, register, verifyEmail, verifyToken} from '../controllers/auth.controller.js';
+import { authRequired } from '../middlewares/validateToken.js';
+import uploadIconProfileImage from '../helpers/multer.config.iconProfile.js';
+import { getProfileImage, uploadProfileImage } from '../controllers/profile.controller.js';
+
 
 const router = express.Router(); //creo una instancia de express.Router()
 
-router.get("/", home);
+router.post('/register', validateSchema(registerSchema), register);
+router.post('/login', validateSchema(loginSchema), login);
+router.post('/logout', logout);
+router.get('/profile', authRequired, profile);
+router.get('/verify-token', verifyToken);
+router.get('/verify-email', verifyEmail);
 
-router.get("/about", about);
 
-router.post("/getUser", getUser); //ruta para obtener el usuario, mando el usuario
+//Estas son las rutas para la imagen de perfil del usuario
+router.post('/upload-profile-image', 
+    authRequired, 
+    uploadIconProfileImage.single("iconProfile"), // 'iconProfile' tiene que coincidir con el name del front del input
+    uploadProfileImage
+); 
+router.get('/profile-image', authRequired, getProfileImage);
+
 
 export default router; //exporto la instancia de express.Router()
